597c7580d795e48bd84b9c2c0cdd303866ddf395
shubhamshivathare <118381699+shubhamshivathare@users.noreply.github.com>
2022-11-17 10:20:37 +0530

    tes
    test

---
+++ test.java
@@ -0,0 +1,16 @@
+//Print the average of three numbers entered by user by creating a class named 'Average' having a method to 
+//calculate and print the average.
+class average{
+    public int avg(int a,int b,int c){
+         int d=(a+b+c/3);
+         return d;
+    } 
+}
+public class Main {
+    public static void name(String[] args) {
+        average a1=new average();
+        a1.avg(20,20,20);
+        System.out.println(d);
+    }
+    
+}
